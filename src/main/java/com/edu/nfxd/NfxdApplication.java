a2edba3f3664d0815746fac99067deb7798e0e10
Administrator <Administrator@SC-201903261747>
2019-06-03 15:47:14 +0800

    nfxd

---
+++ src/main/java/com/edu/nfxd/NfxdApplication.java
@@ -0,0 +1,13 @@
+package com.edu.nfxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfxdApplication.class, args);
+	}
+
+}
